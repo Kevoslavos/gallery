6f77878440ae007da6447e6e56df955569471c68
Kamil Cieslak <kamil.cieslak@mfipr.gov.pl>
2019-12-12 15:06:08 +0100

    update_v11

--- scripts/full-photo.js
+++ scripts/full-photo.js
@@ -1,17 +1,34 @@
 class fullPhoto {
-        size = {
-            width: 1920,
-            height: 1080,
-        };
-        display(){
+    url = null;
+    size = {
 
-        }
-        //metoda ->method
-        close (){
+        //wlasciwosc -> property
 
-        }
+        width: null,
+        height: null,
+    };
+
+    constructor(url, width, height) {
+        this.url = url;
+        this.size.width = width;
+        this.size.height = height;
+    }
+    display() {
+
+    }
+    //metoda ->method
+    close() {
+
+    }
 
 }
 
-const big1 = new fullPhoto();
-const big2 = new fullPhoto();
+const big1 = new fullPhoto(
+    'https://picsum.photos/1920/1080', 1920, 1080
+   
+);
+const big2 = new fullPhoto(
+    'https://picsum.photos/1280/720', 1280, 720
+);
+
+debugger;
